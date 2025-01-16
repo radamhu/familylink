179c64866eddaf94ea2202f226bb86aed5087263
Matthew O'Gorman <mog@rldn.net>
2025-01-16 14:36:20 -0500

    updates all things i need

--- blah.py
+++ blah.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.insert(0, './src/')
 from pprint import pprint
-
+import math
 from familylink import FamilyLink
 import logging.config
 
@@ -51,10 +51,14 @@ def main():
     pprint(children)
     for child in children:
         for device in child['devices']:
-            client.set_time_limits(child['id'],
-                                   device['id'],
-                                   device['limit']['id'],
-                                   device['limit']['minutes'] - 5)
+            client.enable_time_limits_device(child['id'], device['id'], device['limit']['id'], device['limit']['minutes'])
+            # client.turn_on_downtime_device(child['id'],
+            #                                 device['id'],
+            #                                 device['down_time']['starts_at']['hour'],
+            #                                 device['down_time']['starts_at']['minute'],
+            #                                 device['down_time']['ends_at']['hour'],
+            #                                 device['down_time']['ends_at']['minute'],
+            #                                 device['down_time']['id'])
     for child in children:
         get_children_time_limits(child)
     pprint(children)
@@ -62,20 +66,36 @@ def main():
 def get_children_time_limits(child):
     time_limits = client.get_time_limits(child['id'])
     applied_time_limits = time_limits['appliedTimeLimits']
+    pprint(applied_time_limits)
     devices = []
     for limit in applied_time_limits:
         device = {}
+
         device['limit'] = {}
+        key = 'currentUsageLimitEntry'
+        if 'inactiveCurrentUsageLimitEntry' in limit:
+            key = 'inactiveCurrentUsageLimitEntry'
+            device['limit']['id'] = limit[key]['id']
+            device['limit']['minutes'] = limit[key]['usageQuotaMins']
+            device['limit']['state'] = limit[key]['state']
+            device['remaining_time'] = 0
+            device['spent_time'] = math.ceil(int(limit['currentUsageUsedMillis'])/60000)
+        else:
+            device['limit']['id'] = limit[key]['id']
+            device['limit']['minutes'] = limit[key]['usageQuotaMins']
+            device['limit']['state'] = limit[key]['state']
+            device['remaining_time'] = limit['currentUsageRemainingMins']
+            device['spent_time'] = device['limit']['minutes'] - device['remaining_time']
+
         device['down_time'] = {}
-        device['limit']['id'] = limit['currentUsageLimitEntry']['id']
-        device['limit']['minutes'] = limit['currentUsageLimitEntry']['usageQuotaMins']
-        device['limit']['state'] = limit['currentUsageLimitEntry']['state']
-        device['down_time']['id'] = limit['todayWindowLimitEntry']['id']
-        device['down_time']['state'] = limit['todayWindowLimitEntry']['state']
-        device['down_time']['start_at'] = {'hour': limit['todayWindowLimitEntry']['startsAt']['hour'], 'minute': limit['todayWindowLimitEntry']['startsAt']['minute']}
-        device['down_time']['ends_at'] = {'hour': limit['todayWindowLimitEntry']['endsAt']['hour'], 'minute': limit['todayWindowLimitEntry']['endsAt']['minute']}
-        device['remaining_time'] = limit['currentUsageRemainingMins']
-        device['spent_time'] = device['limit']['minutes'] - device['remaining_time']
+        key ='todayWindowLimitEntry'
+        if 'inactiveTodayWindowLimitEntry' in limit:
+            key = 'inactiveTodayWindowLimitEntry'
+        device['down_time']['id'] = limit[key]['id']
+        device['down_time']['state'] = limit[key]['state']
+        device['down_time']['starts_at'] = {'hour': limit[key]['startsAt']['hour'], 'minute': limit[key]['startsAt']['minute']}
+        device['down_time']['ends_at'] = {'hour': limit[key]['endsAt']['hour'], 'minute': limit[key]['endsAt']['minute']}
+
         device['id'] = limit['deviceId']
         devices.append(device)
         child['devices'] = devices
